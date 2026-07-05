3c41a757c204871c8a5e20a3fa48774190e9c850
qezman <holaryinka5050@gmail.com>
2026-07-05 18:43:36 +0100

    fix: scale nodes to 5

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -38,17 +38,17 @@ variable "node_instance_type" {
 variable "node_desired_size" {
   description = "Desired number of worker nodes"
   type        = number
-  default     = 4
+  default     = 5
 }
 
 variable "node_min_size" {
   description = "Minimum number of worker nodes"
   type        = number
-  default     = 1
+  default     = 2
 }
 
 variable "node_max_size" {
   description = "Maximum number of worker nodes"
   type        = number
-  default     = 4
+  default     = 5
 }
